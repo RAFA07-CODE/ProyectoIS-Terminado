62060e6a1f21f07a0855b07a64aaed3e30e89dc6
sofiarayo07 <ramirez.garcia.sofia1jvesp@gmail.com>
2025-06-03 18:25:40 -0600

    autorizacion bd

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -4,7 +4,15 @@ dotenv.config()
 
 const db = new Sequelize(process.env.DATABASE_URL!, {
     models: [__dirname + '/../models/**/*'],
-    logging: false
+    logging: false,
+    dialect: 'postgres',
+    protocol: 'postgres',
+    dialectOptions: {
+        ssl:{
+            require: true,
+            rejectUnanuthorized: false
+        }
+    }
 })
 
 export default db
